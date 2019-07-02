c49390aac5a4e91078c671feb103a0dd6d5e81b7
Bryan <guamanbryan1995@hotmail.com>
2019-07-02 02:15:14 +0100

    version

---
+++ Datos.java
@@ -0,0 +1,22 @@
+public class Datos {
+    private  float valor;
+   
+    public Datos(float valor) {
+        this.valor = valor;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    public float sumaValor(float valor,float valor1){
+        
+        return valor+valor1;
+    }
+  
+
+}
